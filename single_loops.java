e1a26efda065ed9cb433b02235bf15c772da5d55
CodeTechGuy <vishalprajwal.s@gmail.com>
2026-02-04 23:26:31 +0530

    hollowDiamond

--- single_loops.java
+++ single_loops.java
@@ -132,4 +132,42 @@ public class single_loops {
             System.out.println();
         }
     }
+
+    public static void hollowDiamond(){
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the number: ");
+            int n = sc.nextInt();
+    
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=n-i;j++){
+                    System.out.print(" ");
+                }
+                for(int j=1;j<=i*2-1;j++){
+                    if(i==n || j==1 || j==2*i-1){
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+    
+            for(int i=n-1;i>=1;i--){
+                for(int j=1;j<=n-i;j++){
+                    System.out.print(" ");
+                }
+                for(int j=1;j<=i*2-1;j++){
+                    if(i==n || j==1 || j==2*i-1){
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+    }
+    
+    
 }
