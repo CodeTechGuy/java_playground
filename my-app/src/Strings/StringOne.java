caf1940f99614dff5125cac72318a1d338e7b50f
CodeTechGuy <vishalprajwal.s@gmail.com>
2026-05-26 23:52:47 +0530

    String basics

---
+++ my-app/src/Strings/StringOne.java
@@ -0,0 +1,12 @@
+package Strings;
+
+//import java.util.Strings;
+public class StringOne {
+
+    public static void main(String[] args) {
+        String str = "hello all";
+        char ch = str.charAt(3);
+        System.out.println(ch);
+    }
+
+}
