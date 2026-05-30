56e57ea718d2a00baf6fc47b97a0aad55f87d68e
CodeTechGuy <vishalprajwal.s@gmail.com>
2026-05-30 22:40:27 +0530

    count of palindrome substring

--- my-app/src/Strings/palindrome_count.java
+++ my-app/src/Strings/palindrome_count.java
@@ -3,13 +3,28 @@ package Strings;
 public class palindrome_count {
 
     public static void main(String[] args) {
-        String str = "abcd";
+        String str = "abcdcba";
         int n = str.length();
+        int count = 0;
         for(int i=0 ; i<n ; i++){
             for(int j=i+1; j<=n ;j++){
-                System.out.print(str.substring(i,j)+" ");
+                boolean ans = palindrome(str.substring(i,j));
+                if(ans == true){
+                    count++;
+                }
             }
         }
+        System.out.println("Number of palindrome substring is "+count);
+    }
+    public static boolean palindrome(String str){
+
+        int n = str.length();
+        for (int i = 0; i < n / 2; i++) {
+            if (str.charAt(i) != str.charAt(n - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
